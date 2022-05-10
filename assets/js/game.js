07084d5e311ab83458a9081add7d7bd8b67a7655
Zach Fulmer <Zachfulmer@earthlink.net>
2022-05-09 19:34:07 -0500

    played with js

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,19 @@
-window.alert("This is an alert! Javascript is running!")
+// This creates a function called fight
+
+function fight()
+{
+    window.alert("The fight has begun!");
+}
+
+//fight();
+
+var playername = window.prompt("what is your robot's name?");
+
+console.log(playername);
+
+console.log("This logs a string, good for leaving yourself a message!");
+
+//this will do math and log 20
+console.log(10+10);
+//what is this?
+console.log("Our robot's name is " + playername);
